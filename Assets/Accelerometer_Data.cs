7befbfb3c9f7bff8beb3f389af5e0f8baf49070a
Bharath <krishnanbh96@gmail.com>
2019-03-08 09:04:00 -0500

    Data Aquisiton working
    TCP communication working b/w microcontroller and accelerometer.
    
    -Need to add cube movement

---
+++ Assets/Accelerometer_Data.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer_Data : MonoBehaviour
+{
+    Data_Aquisition data;
+    // Start is called before the first frame update
+    void Start()
+    {
+        data = new Data_Aquisition();
+        data.Begin("192.168.4.1", 80);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Data_Aquisition.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Globalization;
+using UnityEngine;
+
+public class Data_Aquisition : MonoBehaviour
+{
+    public float Accelx , Gyrox;
+    public float Accely , Gyroy;
+    public float Accelz , Gyroz;
+    
+
+    // Start is called before the first frame update
+    public void Begin(string IP, int Port)
+    {
+        var thread = new Thread(() =>
+        {
+            // Declaring Client
+            var client = new TcpClient();
+
+            // Creating Client on specified IP adress and Port
+            client.Connect(IP, Port);
+            var stream = new StreamReader(client.GetStream());
+
+            //Declaring Temporary Buffer
+            var buffer = new List<byte>();
+
+            while (client.Connected)
+            {
+                // Reads data stream
+                var data = stream.Read();
+
+                if (data == 13)
+                {
+                    // Converting Incoming ASCII Values to string
+                    var incommingData = Encoding.ASCII.GetString(buffer.ToArray());
+                    // Display Data
+                    Debug.Log("Data Recieved:" + incommingData);
+                    // Spliting Data into appropriate variables
+                    string[] all_data = incommingData.Split(',');
+                    
+                    // Conerting String to Float data
+                    Accelx = float.Parse(all_data[0], CultureInfo.InvariantCulture.NumberFormat);
+                    Accely = float.Parse(all_data[1], CultureInfo.InvariantCulture.NumberFormat);
+                    Accelz = float.Parse(all_data[2], CultureInfo.InvariantCulture.NumberFormat);
+                    Gyrox = float.Parse(all_data[3], CultureInfo.InvariantCulture.NumberFormat);
+                    Gyroy = float.Parse(all_data[4], CultureInfo.InvariantCulture.NumberFormat);
+                    Gyroz = float.Parse(all_data[5], CultureInfo.InvariantCulture.NumberFormat);
+
+                    buffer.Clear();
+
+                    
+                }
+                else
+                    // Continue reading incomming data
+                    buffer.Add((byte)data);
+
+             
+
+            }
+
+            client.GetStream().Close();
+            client.Close();
+                       
+        });
+        thread.Start();
+    }
+}
+   
