5d752ad84717f5512ab40c7a44a03c694e8d6646
Bharath <krishnanbh96@gmail.com>
2019-03-19 22:36:03 -0400

    Flying Objects w/ Random Spawning
    To Do:
    - Add Collision detection between player and object flying
    - Add different objects that fly differently
    - Add Powerups/ Coins for score???

---
+++ Assets/Object_Collider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object_Collider : MonoBehaviour
+{
+    float moveSpeed = 10f;
+    float rotateSpeed = 30f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, moveSpeed * Time.deltaTime);
+        transform.Rotate(0, 0, rotateSpeed * Time.deltaTime, Space.Self);
+    }
+}
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public GameObject spawnee;
+    public bool stopSpawning = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    private void Start()
+    {
+        /// Adds the start time and final time to the spawn object
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+
+
+    }
+
+    public void SpawnObject()
+    {
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if (stopSpawning)
+        {
+            CancelInvoke("SpawnObject");
+
+        }
+
+    }
+
+
+}
