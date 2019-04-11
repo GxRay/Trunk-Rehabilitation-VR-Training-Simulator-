96eb8592f4030ced50f2c2de7f03f2b36c44f951
Bharath <krishnanbh96@gmail.com>
2019-04-11 18:52:33 -0400

    fixed spawning

--- Assets/WaveSpawner.cs
+++ Assets/WaveSpawner.cs
@@ -32,9 +32,25 @@ public class WaveSpawner : MonoBehaviour
         } 
         else
         {
+            bool same = true;
             // Randomizes Spawn Area for Objects between Back Wall (FOR NOW)
             float SpawnZone_X = Random.Range(Player.transform.position.x - 2f, Player.transform.position.x + 2f);
-            float SpawnZone_Y = Random.Range(Player.transform.position.y - 4f, Player.transform.position.y + 1f);
+            float SpawnZone_Y = Random.Range(Player.transform.position.y - 3f, Player.transform.position.y + 1f);
+            while (same)
+            {
+                if (SpawnZone_X >= Player.transform.position.x - 0.5 && SpawnZone_X <= Player.transform.position.x + 0.5)
+                {
+                    SpawnZone_X = Random.Range(Player.transform.position.x - 1.5f, Player.transform.position.x + 1.5f);
+                }
+                if (SpawnZone_Y >= Player.transform.position.y - 1 && SpawnZone_Y <= Player.transform.position.y + 0.263)
+                {
+                    SpawnZone_Y = Random.Range(Player.transform.position.y - 3f, Player.transform.position.x + 1f);
+                }
+                else
+                {
+                    same = false;
+                }
+            }
             float SpawnZone_Z = 25.02f;
             SpawnZone = new Vector3(SpawnZone_X, SpawnZone_Y, SpawnZone_Z);
             Instantiate(spawnee, SpawnZone, transform.rotation);
